fa161c9a375913e3e8c6377df1679cedb994786e
hazargezici <hazargezici82@gmail.com>
2022-08-24 16:25:29 +0300

    timeline
    timeline with vuejs

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+    <TimeLine :timelineData="timelineData" />
+    <div class="form-group">
+      <input
+        v-model="item"
+        class="input"
+        type="text"
+        placeholder="Add Item"
+        @keyup.enter="addItem"
+      />
+      <button class="btn" @click="addItem">Add</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import TimeLine from "./components/TimeLine";
+
+export default {
+  name: "App",
+  components: {
+    TimeLine,
+  },
+  data() {
+    return {
+      item: "",
+      timelineData: [],
+    };
+  },
+  methods: {
+    addItem() {
+      const payload = {
+        date: new Date(),
+        desc: this.item,
+      };
+
+      this.timelineData.push(payload);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: sans-serif;
+  background-color: #ecf4f4;
+  color: #fff;
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+.form-group {
+  margin-bottom: 20px;
+  & .input {
+    height: 40px;
+    padding: 3px 5px;
+    border: none;
+    outline: none;
+  }
+  & .btn {
+    height: 40px;
+    border: none;
+    border-radius: 5px;
+    background-color: #fff;
+    margin-left: 5px;
+    padding: 5px;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
+</style>
---
+++ src/components/TimeLine.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="waves">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#F9C35C"
+            fill-opacity="1"
+            d="M0,288L48,261.3C96,235,192,181,288,181.3C384,181,480,235,576,261.3C672,288,768,288,864,277.3C960,267,1056,245,1152,245.3C1248,245,1344,267,1392,277.3L1440,288L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
+        </svg>
+      </div>
+    </div>
+    <div class="timeline-container">
+      <div class="blur">
+        <h3>Updates</h3>
+        <p>23 in last 7 hours</p>
+      </div>
+      <div class="timeline" v-if="timelineData.length > 0">
+        <ul class="timeline-list">
+          <template v-for="item in timelineData">
+            <li class="timeline-item" :key="item">
+              <div class="timeline-item-content">
+                <b class="timeline-item-date">{{ getDate(item.date) }}</b>
+                <p class="timeline-item-desc">{{ item.desc }}</p>
+              </div>
+            </li>
+          </template>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  name: "TimeLine",
+  props: {
+    timelineData: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    getDate(date) {
+      return moment(date).hours() + ":" + moment(date).minutes();
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  & .card {
+    background-color: #fb748c;
+    height: 160px;
+    width: 360px;
+    border-radius: 10px;
+    svg {
+      border-radius: 10px;
+    }
+  }
+  & .timeline-container {
+    height: 100%;
+    width: 300px;
+    margin: auto;
+    position: relative;
+    top: -100px;
+    border-radius: 5px;
+    .blur {
+      height: 100px;
+      background-color: rgba(255, 255, 255, 0.4);
+      -webkit-backdrop-filter: blur(5px);
+      backdrop-filter: blur(5px);
+      border-top-right-radius: 5px;
+      border-top-left-radius: 5px;
+      line-height: 30px;
+      padding-top: 10px;
+      padding-left: 30px;
+      color: #fff;
+    }
+    .timeline {
+      background-color: #fff;
+      border-bottom-right-radius: 5px;
+      border-bottom-left-radius: 5px;
+      color: black;
+      position: relative;
+      &:before {
+        content: "";
+        position: absolute;
+        height: 100%;
+        width: 2px;
+        top: 50%;
+        left: 15%;
+        transform: translate(-50%, -50%);
+        background-color: #a1a0b9;
+      }
+
+      &-list {
+        list-style: none;
+      }
+
+      &-item {
+        padding: 25px 5px;
+        background-color: #fff;
+        box-shadow: 2px 2px 10px 2px rgba(0, 0, 0, 0.2);
+        border-radius: 5px;
+        width: 70%;
+        position: relative;
+        top: -30px;
+        left: -60px;
+        padding-left: 25px;
+        &:before {
+          content: "";
+          position: absolute;
+          top: 30px;
+          border-radius: 50%;
+          background-color: #a1a0b9;
+          height: 20px;
+          width: 20px;
+        }
+        transform: translateX(60%);
+        &:before {
+          left: -31px;
+        }
+
+        &:not(:last-child) {
+          margin-bottom: 30px;
+        }
+        &-date {
+          font-size: 14px;
+        }
+        &-desc {
+          font-size: 15px;
+          overflow: auto;
+        }
+      }
+    }
+  }
+}
+</style>
